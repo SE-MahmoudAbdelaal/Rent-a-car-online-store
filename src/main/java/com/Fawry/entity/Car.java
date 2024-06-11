04f2f78cb5033b7c68cfd6034212cc1a84fa2308
Mahmoud Abdelaal <mahmoud.abdelaal.dev@gmail.com>
2024-06-11 17:34:54 +0300

    Add Car entity to represent car details

---
+++ src/main/java/com/Fawry/entity/Car.java
@@ -0,0 +1,73 @@
+package com.Fawry.entity;
+
+
+
+
+
+public class Car {
+private int id;
+private String name;
+private int price;
+private String description;
+private String image;
+
+    public Car() {
+    }
+
+
+
+    public Car(String name, int price, String description, String image) {
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.image = image;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public int getId() {
+        return id;
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}
